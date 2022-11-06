540d6347ae9091f0c5c506d9d82ddb4a8daba33e
Kwanghoon Choi <lazyswamp@gmail.com>
2022-11-06 17:10:59 +0900

    update table.h and tablebak.ht in exercise

--- 09_template_stl_and_lambda/exercise/template_table/Table.h
+++ 09_template_stl_and_lambda/exercise/template_table/Table.h
@@ -7,10 +7,10 @@ template <class T>
 class Table {
 public:
 	Table(); // 기본 생성자
-	Table(const Table& tbl); // 복사 생성자
+	Table(const Table<T>& tbl); // 복사 생성자
 	Table(int size); // 일반 생성자
 	~Table(); // 소멸자
-	const Table& operator=(const Table& table); // 할당 연산자
+	const Table& operator=(const Table<T>& table); // 할당 연산자
 	T& operator[](int index); // 배열 연산자
 protected:  // protected
 	int size; // 배열의 크기
