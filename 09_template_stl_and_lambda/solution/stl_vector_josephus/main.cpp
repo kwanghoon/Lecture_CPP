820befe5fe5defda548ff5db7581b2a49cab5312
Kwanghoon Choi <lazyswamp@gmail.com>
2022-11-19 21:34:26 +0900

    vectorl josephus

---
+++ 09_template_stl_and_lambda/solution/stl_vector_josephus/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+  int numSoldiers;
+
+  cout << "Enter the number of soldiers" << endl;
+  cin >> numSoldiers;
+
+  vector<int> soldiers(numSoldiers);
+
+  for (int i=0; i<numSoldiers; i++) soldiers[i] = i+1;
+
+  if (numSoldiers < 1)
+    cout << 0 << endl;
+  else if (numSoldiers == 1)
+    cout << 1 << endl;
+  else {
+    int current;
+
+    current = 0;
+
+    while (soldiers.size() >  1) {
+      current++;
+
+      if (current == soldiers.size())
+	current = 0;
+
+      // for (int j=0; j<soldiers.size(); j++)
+      // cout << soldiers[j] << " ";
+      // cout << endl;
+      
+      soldiers.erase(soldiers.begin() + current);
+
+      if (current ==soldiers.size())
+	current = 0;
+    }
+
+    cout << soldiers[0] << endl;
+    
+  }
+  
+  return 0;
+}
